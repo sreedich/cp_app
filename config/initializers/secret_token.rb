5912dd41adea304d377b2c34868b33d122bfbd53
Steven Reedich <sreedich@gmail.com>
2013-07-31 15:44:53 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrashpadApp::Application.config.secret_token = '052b20b58936a9a9db48da5ad36371a066af100421d753e931f36e2d4fe53ac7936801c33833a76bfc026ab9ce275826c0106f2f1546441a3f33834cdec18f58'
