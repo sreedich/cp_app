9ecd8b7b5e53bde07e422f38733e65ad7c6e66be
Jide Fajobi <wazogear@gmail.com>
2013-08-01 10:09:29 -0400

    add admin login feature [#54402290]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 CrashpadApp::Application.routes.draw do
-
   devise_for :users 
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
+  
   resources :users 
   # The priority is based upon order of creation:
   # first created -> highest priority.
