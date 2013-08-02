daf6ebea5e15ed4aebebdd44ae9ebc93b9666f94
Steven Reedich <sreedich@gmail.com>
2013-08-02 16:11:23 -0400

    category index

--- spec/features/Category/admin_creates_category_and_items_spec.rb
+++ spec/features/Category/admin_creates_category_and_items_spec.rb
@@ -2,14 +2,29 @@ require 'spec_helper.rb'
 
 
 describe 'admin creates a category with items' do 
+  before(:each) do 
+    sign_up_as(user)
+    visit new_category_path 
+  end
 
  let(:user) {FactoryGirl.create(:user)}
 
-  it 'creates a category' do
-    visit new_user_registration_path 
-    sign_up_as(user)
-    visit categories_path 
+  it 'visit category' do
     expect(page).to have_content('Category')
   end 
+
+   it 'creates a category' do
+    previous_count = Category.count 
+    fill_in 'Title', with: 'Kitchen'
+    click_on 'Submit'
+    expect(Category.count).to eql(previous_count + 1)
+  end 
+
+  it 'list categories on index page' do
+    fill_in 'Title', with: 'Kitchen'
+    click_on 'Submit'
+    expect(page).to have_content('Kitchen')
+  end 
+
 
 end
